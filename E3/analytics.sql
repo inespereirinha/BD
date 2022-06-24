d4fd263776318e4c41bfd8c4881b4da19611ed5c
Teresa Correia <tgalvaoc@users.noreply.github.com>
2022-06-24 20:40:20 +0100

    Update analytics.sql

--- E3/analytics.sql
+++ E3/analytics.sql
@@ -1,16 +1,5 @@
 /* OLAP */
-/*
-Usando a vista desenvolvida para a Questão 4, escreva duas consultas SQL que permitam analisar o 
-número total de artigos vendidos:
-1. num dado período (i.e. entre duas datas), por dia da semana, por concelho e no total*/
-
-SELECT dia_semana, concelho, SUM (unidades) as total
-FROM vendas
-WHERE make_date(CAST(ano AS INTEGER), CAST(mes AS INTEGER), CAST(dia_mes AS INTEGER)) BETWEEN '2020-1-1' AND '2020-12-31'
-GROUP BY
-GROUPING SETS(dia_semana, concelho, ()); 
-
-/* OUUUUUUUUUUUUUUUU GROUPING SETS */
+/* 1. num dado período (i.e. entre duas datas), por dia da semana, por concelho e no total */
 
 SELECT dia_semana, concelho, SUM (unidades) as total
 FROM vendas
@@ -18,15 +7,7 @@ WHERE make_date(CAST(ano AS INTEGER), CAST(mes AS INTEGER), CAST(dia_mes AS INTE
 GROUP BY
     CUBE(dia_semana, concelho)
 
-/*2. num dado distrito (i.e. "Lisboa"), por concelho, categoria, dia da semana e no total */
-
-SELECT distrito, concelho, cat, dia_semana, SUM (unidades) as total
-FROM vendas
-WHERE distrito = 'Lisboa'
-GROUP BY 
-GROUPING SETS((distrito, concelho), (distrito, cat), (distrito, dia_semana), ()); 
-
-/* OUUUUUU CUBE PROB */
+/* 2. num dado distrito (i.e. "Lisboa"), por concelho, categoria, dia da semana e no total */
 
 SELECT concelho, cat, dia_semana, SUM (unidades) as total
 FROM vendas
